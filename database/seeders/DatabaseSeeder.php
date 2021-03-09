6d4e161f2dd6885ff0635b88f5407b56d09c2cc2
lena-almahdi <lenaalmahdi54321@gmail.com>
2021-03-09 12:30:48 +0200

    add factory to area and address models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\Meal;
 use App\Models\Restaurant;
+use App\Models\Address;
+use App\Models\Area;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +20,7 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleTableSeeder::class);
         Restaurant::factory(10)->create();
         Meal::factory(50)->create();
+        Address::factory(30)->create();
+        Area::factory(25)->create();
     }
 }
