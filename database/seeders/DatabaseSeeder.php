a98641dbe039779eddd526d13efcb913727e6bb7
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2021-03-09 13:38:44 +0200

    fix DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,12 +4,10 @@ namespace Database\Seeders;
 
 use App\Models\Meal;
 use App\Models\Restaurant;
-<<<<<<< HEAD
 use App\Models\OrderItem;
-=======
+use App\Models\Order;
 use App\Models\Address;
 use App\Models\Area;
->>>>>>> 568abd31af7efa78ed5e9558ca22acb0460d87e2
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -24,12 +22,9 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleTableSeeder::class);
         Restaurant::factory(10)->create();
         Meal::factory(50)->create();
-<<<<<<< HEAD
         OrderItem::factory(50)->create();
-=======
         Address::factory(30)->create();
         Area::factory(25)->create();
         Order::factory(25)->create();
->>>>>>> 568abd31af7efa78ed5e9558ca22acb0460d87e2
     }
 }
