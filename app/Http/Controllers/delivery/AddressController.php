306598ece92c148ab1315a2ec32bfc0dccda7a77
mhd nour <mohamadnourkuzez@gmail.com>
2021-03-14 21:42:12 +0200

    add address delivery

---
+++ app/Http/Controllers/delivery/AddressController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers\delivery;
+
+use App\Http\Controllers\Controller;
+use App\Models\Address;
+use App\Models\Area;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Delivery;
+
+class AddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $addresses= Auth::user()->addresses;
+        return view('delivery.addresses.index', ['addresses' => $addresses]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $areas = Area::all()->pluck('id', 'name');
+
+        return view('delivery.addresses.create', ['areas' => $areas]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // TODO: Get this done please ;)
+        $request->validate([]);
+
+        Auth::user()->addresses()->create($request->only(['city', 'area_id', 'street', 'details']));
+        $user_id=Auth::user()->id;
+        $delivery= delivery::where('user_id',$user_id)->first();
+        
+        return redirect()->route('delivery.show', $delivery->id);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $address=Auth::user()->address;
+        return view('delivery.addresses.show', ['address' => $address]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $address=Auth::user()->address;
+        $address = Address::find($id);
+
+        return view('delivery.addresses.edit', ['address' => $address]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $address = Address::find($id);
+
+        $address->city = $request->city;
+        $address->street = $request->street;
+        $address->details = $request->details;
+        if($address->save()) {
+            request()->session()->flash('success', 'address was updated successfully.');
+        } else {
+            request()->session()->flash('danger', 'Something went wrong.');
+        }
+        return redirect()->route('delivery.addresses.index');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        
+    }
+}
---
+++ app/Http/Controllers/restaurant/AddressController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Customer;
+
+use App\Http\Controllers\Controller;
+use App\Models\Address;
+use App\Models\Area;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AddressController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $addresses= Auth::user()->addresses;
+        return view('customers.addresses.index', ['addresses' => $addresses]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $areas = Area::all()->pluck('id', 'name');
+
+        return view('customers.addresses.create', ['areas' => $areas]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // TODO: Get this done please ;)
+        $request->validate([]);
+
+        Auth::user()->addresses()->create($request->only(['city', 'area_id', 'street', 'details']));
+        
+        return redirect()->back()->with('success', 'done dude');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $address=Auth::user()->address;
+        return view('customers.addresses.show', ['address' => $address]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $address=Auth::user()->address;
+        $address = Address::find($id);
+
+        return view('customers.addresses.edit', ['address' => $address]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $address = Address::find($id);
+
+        $address->city = $request->city;
+        $address->street = $request->street;
+        $address->details = $request->details;
+        if($address->save()) {
+            request()->session()->flash('success', 'address was updated successfully.');
+        } else {
+            request()->session()->flash('danger', 'Something went wrong.');
+        }
+        return redirect()->route('customers.addresses.index');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        
+    }
+}
