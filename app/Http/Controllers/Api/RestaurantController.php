1aebb4b94f0f36f2db6562a6517262f0b4f1a8c1
Roduan Kareem Aldeen <roduan98@gmail.com>
2021-03-14 19:32:15 +0200

    demonstrate api

---
+++ app/Http/Controllers/Api/RestaurantController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\RestaurantResource;
+use App\Models\Restaurant;
+use Illuminate\Http\Request;
+
+class RestaurantController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return RestaurantResource::collection(Restaurant::paginate(5));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $restaurant = Restaurant::create($request->only(['name']));
+        return new RestaurantResource($restaurant);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Restaurant  $restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Restaurant $restaurant)
+    {
+        return new RestaurantResource($restaurant);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Restaurant  $restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Restaurant $restaurant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Restaurant  $restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Restaurant $restaurant)
+    {
+        // $restaurant->delete();
+        return [
+            'message'   => 'Restaurant was deleted'
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AuthenticationController;
+use App\Http\Controllers\Api\RestaurantController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:api')->group(function () {
+});
+Route::post('/login', [AuthenticationController::class, 'login']);
+
+// Route::get('/restaurants', [RestaurantController::class, 'index']);
+Route::resource('restaurants', RestaurantController::class);
